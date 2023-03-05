86b54d0bc662c1ad877c1b72968e16e1bcb1179a
Allan Aoudji <allanaoudji@air-de-allan.home>
2023-03-05 22:51:42 +0100

    create a socket instance and test it

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
+import { socket } from "@src/socket";
 import express from "express";
+import http from "http";
 
 const app = express();
+const server = http.createServer(app);
+const io = socket(server);
 
 app.get("/", (_, res) => {
-  res.status(200).send("Hello world!");
+  res.send({
+    hello: "world",
+  });
 });
 
-export { app };
+export { io, server };
--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,7 @@
-import type https from "http";
+import type http from "http";
 import { Server } from "socket.io";
 
-export const socket = (httpServer: https.Server): Server => {
+export const socket = (httpServer: http.Server): Server => {
   const io = new Server(httpServer, {
     cors: {
       origin: "*",
